a0e85c0451a520ec3bbae98ec113364b95becea2
Tahirintsoa Ulrich <rtfulrich@gmail.com>
2020-07-05 10:01:54 +0300

    View For Authentications | Packages installed: adminlte,livewire,tailwindCSS,turbolinks

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('title') {{$title ?? env('APP_NAME') }} @endsection
+
+@section('body')
+    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+        @yield('content')
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::view('login', 'auth.login')->name('login');
+    // Route::livewire('/login', 'auth.login')->name('login')->layout('layouts.auth')->section('body', ['title' => 'login is title']);
+    Route::view('register', 'auth.register')->name('register');
+    // Route::livewire('/register', 'auth.register')->name('register')->layout('layouts.auth', ['title' => 'Register is title'])->section('body');
+});
+
+Route::view('password/reset', 'auth.passwords.email')->name('password.request');
+Route::get('password/reset/{token}', 'Auth\PasswordResetController')->name('password.reset');
+
+Route::middleware('auth')->group(function () {
+    Route::view('email/verify', 'auth.verify')->middleware('throttle:6,1')->name('verification.notice');
+    Route::get('email/verify/{id}/{hash}', 'Auth\EmailVerificationController')->middleware('signed')->name('verification.verify');
+
+    Route::post('logout', 'Auth\LogoutController')->name('logout');
+
+    Route::view('password/confirm', 'auth.passwords.confirm')->name('password.confirm');
 });
